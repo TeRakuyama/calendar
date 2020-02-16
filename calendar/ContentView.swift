c92f5f524b2f7779f2c8dd5ceef56f891106df41
TeRakuyama <t.rakuyama929t@gmail.com>
2020-02-16 17:54:33 +0900

    Initial Commit

---
+++ calendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calendar
+//
+//  Created by 樂山哲也 on 2020/02/16.
+//  Copyright © 2020 rakuyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
